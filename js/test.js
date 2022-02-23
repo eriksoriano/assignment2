45df0edcbf4de3e936954d513f303302074b92d0
Erik Soriano <83310824+eriksoriano@users.noreply.github.com>
2022-02-23 16:49:08 -0500

    fixed nav

---
+++ js/test.js
@@ -0,0 +1,52 @@
+// --- test alert below
+// alert("is working")
+console.log("This is a string");
+console.log(4 +5);
+
+let num = 7;
+let dos = 3;
+console.log(num + dos); // use varriable to add/subtract content 
+console.log(16 % 3); // should give 1
+console.log(+"10" + 2);
+console.log(2>9);
+// ++ means ------- let num = num + 1;
+
+
+
+// *********************************************************
+
+
+const myVariable = "this is my variable bro";
+console.log(myVariable);
+
+
+// ******** Difference b/w (=) and (+) ******************
+// ******** a !=1; ******************
+
+const myNumero = 1;
+console.log("4" +3);
+
+
+// ************ CONCATENATE is process of (+) ************************
+
+// ******* RE-DECLARE variables *******************
+
+
+let thisVar = 8;
+thisVar = 10;
+console.log(thisVar);
+
+// LET - can be given a new a value but without "let"
+// CONST -  constant and cannot be given new value; cannot be re-declared either 
+
+const firstOne = 1,
+secondOne = 2;
+thirdOne = 3;
+fourOne = 4;
+
+// ******************* FUNCTIONS ********************
+
+
+const year = 2022;
+console.log("this is " + year);
+console.log(typeof year);

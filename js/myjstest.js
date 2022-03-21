c7c9e2cf2b22de0f80b8fbc48991c1b7b75de9ea
Erik Soriano <83310824+eriksoriano@users.noreply.github.com>
2022-03-21 16:29:09 -0400

    removed unneccesary items
    removed:
    hers.css and hers.js

--- js/myjstest.js
+++
@@ -1,5 +0,0 @@
-const blackSection = document.querySelector(".myClass")
-// // Using the Style Property 
-blackSection.style.background = "#efefef"
-blackSection.style.color = "#ff0000"
-blackSection.style.fontSize = "2rem"
